98530fa3b0279aa41cfd5fa65d8b320d86fc4fe1
bonface nyatangi <bonfacenyatangi@gmail.com>
2023-05-22 15:17:31 +0300

    Getter and Setter methods

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
+    # Name setter method
+    def name=(name)
+        @name = name
+    end
+    # Name setter method
+    def name
+        @name
+    end
+# Define a breed getter and a breed= setter method for the instance variable @breed.
+    def breed=(breed)
+        @breed = breed
+    end
 
+    def breed
+        @breed
+    end
 end
